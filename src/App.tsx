4623aefe2203bc538d8436cbf76da8085ebcf59e
Pavel Ignatev <palllkaignatev@gmail.com>
2023-01-21 00:58:59 +0500

    Убрал запрос к api

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,9 @@ import { IntlProvider } from 'react-intl';
 import { LOCALES } from '18n/locales';
 import { messages } from '18n/messages';
 import Pages from 'pages';
-import { useEffect } from 'react';
-import axiosInstance from 'utilities/axiosInstance';
 
 const App = () => {
   const locale = LOCALES.ENGLISH;
-
-  useEffect(() => {
-    axiosInstance.get('');
-  }, []);
 
   return (
     <IntlProvider messages={messages[locale]} locale={locale} defaultLocale={LOCALES.ENGLISH}>
