fc85693e77cedc0b2dbf939c01f5de05d703743d
Pavel Ignatev <palllkaignatev@gmail.com>
2023-01-20 21:43:27 +0500

    Почистил readme.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-<div>ha</div>, document.getElementById('root'));
+ReactDOM.render(<div>ha</div>, document.getElementById('root'));
