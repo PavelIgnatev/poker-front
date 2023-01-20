1e0b7a32c8869d461f9e298257f768dcecb20074
Pavel Ignatev <palllkaignatev@gmail.com>
2023-01-20 22:12:02 +0500

    Тестирую pre-commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>ha</div>, document.getElementById('root'));
+ReactDOM.render(<div>haha</div>, document.getElementById('root'));
